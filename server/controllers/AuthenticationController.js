5e55caaf387dc6d5c7f5c90c9344fd1f033db99c
Yash Goyal <yashgo0018@gmail.com>
2019-01-13 18:15:20 +0530

    removed logs

--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -2,12 +2,10 @@ const { User } = require("../models");
 
 module.exports = {
   async register(req, res) {
-    console.log(req.body);
     try {
       const user = await User.create(req.body);
       res.send(user.toJSON());
     } catch (err) {
-      console.log(err);
       res.status(400).send({
         error: "This email is already in use"
       });
